5581c561d04e3fe551cdea2e808825f5e54498e1
Mikaila Brown <mbr2201@wgu.edu>
2024-04-09 20:13:28 -0400

    upload project

---
+++ client/src/app/employee.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, signal } from '@angular/core';  //From angular/core Signals are new way of doing things https://angular.io/guide/signals 
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './employee';      //Now our employee interface that was just created is now injectable.
+
+@Injectable({                   //Injectable defines where the above is going to be used and is going to be used in the root
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private url = 'http://localhost:5200';
+  employees$ = signal<Employee[]>([]);          //Create two signals employees$ and employee$
+  employee$ = signal<Employee>({} as Employee);
+ 
+  constructor(private httpClient: HttpClient) { }
+
+  private refreshEmployees() {
+    this.httpClient.get<Employee[]>(`${this.url}/employees`)    //Use httpClient to get /employees from our server 
+      .subscribe(employees => {
+        this.employees$.set(employees);             //Subscribe to This and set our variable  employees$ = signal<Employee[]>([]);
+      });
+  }
+
+  getEmployees() {      //Then this runs the refresh and return employees$
+    this.refreshEmployees();
+    return this.employees$();
+  }
+
+  getEmployee(id: string) {         //This is going to get a single employee (employee$)
+    //The ${id} defines our ID so on the server it is going to return an employee  
+    this.httpClient.get<Employee>(`${this.url}/employees/${id}`).subscribe(employee => {
+      this.employee$.set(employee);  //Subscribe to This and set our variable  employee$ = signal<Employee[]>([]);
+
+      return this.employee$();
+    });
+  }
+
+  createEmployee(employee: Employee) {  //This passes in our employee to this function.  
+    return this.httpClient.post(`${this.url}/employees`, employee, { responseType: 'text' });
+  }
+
+  updateEmployee(id: string, employee: Employee) {
+    return this.httpClient.put(`${this.url}/employees/${id}`, employee, { responseType: 'text' });
+  }
+
+  deleteEmployee(id: string) {
+    return this.httpClient.delete(`${this.url}/employees/${id}`, { responseType: 'text' });
+  }
+}
---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import { connectToDatabase } from "./database";
+import { employeeRouter } from "./employee.routes"; //add this after the last import
+
+// Load environment variables from the .env file, where the ATLAS_URI is configured
+dotenv.config();
+
+const { ATLAS_URI } = process.env;
+// const uri = process.env.ATLAS_URI
+
+if (!ATLAS_URI) {
+  console.error(
+    "No ATLAS_URI environment variable has been defined in config.env"
+  );
+  process.exit(1);
+}
+
+connectToDatabase(ATLAS_URI)
+  .then(() => {
+    const app = express();
+    app.use(cors());
+    app.use("/employees", employeeRouter); //Telling it that the employees and to use the emplyeeRouter
+    // start the Express server: npx ts-node src/server.ts
+    app.listen(5200, () => {
+      console.log(`Server running at http://localhost:5200...`);
+    });
+  })
+  .catch((error) => console.error(error));
+
+  
