a062596b88e22a9186f5d24f3070e4e57cadf27a
Mikaila Brown <mbr2201@wgu.edu>
2024-04-19 11:00:04 -0400

    update route server

--- server/api/index.ts
+++ server/api/index.ts
@@ -27,6 +27,9 @@ connectToDatabase(ATLAS_URI)
     app.use(cors(corsOptions));
     app.use("/employees", employeeRouter); //Telling it that the employees and to use the emplyeeRouter
     // start the Express server: npx ts-node src/server.ts
+    app.get('/', (req, res) => {
+      res.send('connected').status(200)
+    })
     app.listen(5200, () => {
       console.log(`Server running at http://localhost:5200...`);
     });
